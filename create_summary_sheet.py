ffc497276ec0ecaff11fd73f05c7f00fdb1204f6
Christopher Brossman <christopherbrossman@Christophers-MacBook-Pro.local>
2023-08-06 16:21:57 -0700

    updated summary sheet

--- create_summary_sheet.py
+++ create_summary_sheet.py
@@ -3,7 +3,8 @@ import pandas as pd
 from pathlib import Path
 import re
 import openai
-openai.api_key = 'XXX'
+
+openai.api_key = os.getenv('OPENAI_KEY')
 
 def filter_python_code(code):
     # Remove single line comments
@@ -27,34 +28,47 @@ for folder in [d for d in os.listdir() if os.path.isdir(d)]:
     for file in Path(folder).rglob("*.py"):
         with open(file, 'r', encoding='utf-8', errors='ignore') as f:
             code = f.read()
-            filtered_code = filter_python_code(code)
-            data.append([folder, file.name, filtered_code])
+            #code = filter_python_code(code)
+            data.append([folder, file.name, code])
 
 df = pd.DataFrame(data, columns=['folder', 'file', 'code'])
 
 # Filter rows where 'folder' starts with an uppercase letter
 df = df[df['folder'].str[0].str.isupper()]
 
-
-
-def get_summary(code):
-    SYSTEM = """
-    highlight the most important step of this algorithm to remember. 
-    If more than one step list them. But only highlight what may be 
-    considered the most important things to remember. Dont be verbose.  
-    """
+def get_chat_completion(code, sys):
     response = openai.ChatCompletion.create(
     model="gpt-3.5-turbo",
     messages=[
-            {"role": "system", "content": SYSTEM},
+            {"role": "system", "content": sys},
             {"role": "user", "content": '```{}```'.format(code)},
         ]
     )
     return response["choices"][0]["message"]['content']
 
 def get_summary_for_row(row, current, total):
+    SYSTEM = """
+    highlight the most important step of the main algorithm to remember. 
+    If more than one step list them. But only highlight what may be 
+    considered the most important things to remember. Dont be verbose.  
+    """
     try:
-        result = get_summary(row['code'])
+        result = get_chat_completion(str(row), SYSTEM)
+        print(f"Row {current} of {total}: Successfully fetched summary for file {row['file']} in folder {row['folder']}")
+    except Exception as e:
+        print(f"Row {current} of {total}: Error fetching summary for file {row['file']} in folder {row['folder']}: {e}")
+        result = None  # or return an appropriate default value
+    return result
+
+def get_problem_for_row(row, current, total):
+    SYSTEM = """
+    Write a summary of the problem the algorithm is attempting to solve. 
+    Dont be verbose. Summarize in 1-3 sentences For example here is a great output:
+    ```Given an integer array nums of length n and an integer target, 
+    find three integers in nums such that the sum is closest to target.```
+    """
+    try:
+        result = get_chat_completion(str(row), SYSTEM)
         print(f"Row {current} of {total}: Successfully fetched summary for file {row['file']} in folder {row['folder']}")
     except Exception as e:
         print(f"Row {current} of {total}: Error fetching summary for file {row['file']} in folder {row['folder']}: {e}")
@@ -63,5 +77,6 @@ def get_summary_for_row(row, current, total):
 
 total_rows = df.shape[0]
 df['summary'] = [get_summary_for_row(row, i+1, total_rows) for i, row in df.iterrows()]
+df['problem'] = [get_problem_for_row(row, i+1, total_rows) for i, row in df.iterrows()]
 df.drop('code', axis=1, inplace=True)
 df.to_csv('summary.csv', index=False)
